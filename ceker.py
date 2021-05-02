e7e7f6a507dd902239b94fdc39884e0294fe6c4e
Abdi Pranata <39010800+sinonx@users.noreply.github.com>
2021-05-02 19:49:50 +0700

    Add files via upload

---
+++ ceker.py
@@ -0,0 +1,46 @@
+import requests
+from colorama import Fore, Back, Style
+
+
+
+
+def sat():
+
+
+
+	print("""
+
+		#############################################
+		#              SHELL CHECKER                #
+		#                                           #
+		#                                           #
+		#                Thanks to :                #
+		#   Family Attacy Cyber ~ Tatsumi Crew      #
+		#############################################
+
+	""") #Coded by SinonX
+
+	print('\n')
+
+
+	tol = input('Your list shell here => ')
+	yee = open(tol, 'r').readlines()
+	for i in yee:
+		ga = i.strip()
+
+		r = requests.get(ga)
+
+		if r.status_code == 200:
+			print(f'{Fore.GREEN}[+]LIVE => ', ga)
+			open('live.txt', 'a').write(ga+'\n')
+		elif r.status_code == 403:
+			print(f'{Fore.CYAN}[-]FORBIDDEN => ', ga)
+		elif r.status_code == 404:
+			print(f'{Fore.RED}[-]NOT FOUND => ', ga)
+		else:
+			print(f'{Fore.WHITE}[?]UNKNOWN => ', ga)
+
+
+if __name__ == "__main__":
+	sat()
+	
