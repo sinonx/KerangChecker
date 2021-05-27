b99e38489ae7a19eeddd5da1e25afc3d72202e56
Abdi Pranata <39010800+sinonx@users.noreply.github.com>
2021-05-28 03:33:04 +0700

    Update ceker.py

--- ceker.py
+++ ceker.py
@@ -31,18 +31,20 @@ def sat():
 		ga = i.strip()
 		r = requests.get(ga)
 		sat = re.findall('<title>(.*?)</title>', r.text)
-
-		if r.status_code == 200:
-			print(f'{Fore.GREEN}[+]LIVE => ', ga)
-			open('live.txt', 'a').write(ga+'\n')
-			for z in sat:
-				print('Title : ', z)
-		elif r.status_code == 403:
-			print(f'{Fore.CYAN}[-]FORBIDDEN => ', ga)
-		elif r.status_code == 404:
-			print(f'{Fore.RED}[-]NOT FOUND => ', ga)
-		else:
-			print(f'{Fore.WHITE}[?]UNKNOWN => ', ga)
+		try:
+			if r.status_code == 200:
+				print(f'{Fore.GREEN}[+]LIVE => ', ga)
+				open('live.txt', 'a').write(ga+'\n')
+				for z in sat:
+					print('Title : ', z)
+			elif r.status_code == 403:
+				print(f'{Fore.CYAN}[-]FORBIDDEN => ', ga)
+			elif r.status_code == 404:
+				print(f'{Fore.RED}[-]NOT FOUND => ', ga)
+			else:
+				print(f'{Fore.WHITE}[?]UNKNOWN => ', ga)
+		except Exception:
+				print(f'{Fore.WHITE}[?]UNKNOWN => ', ga)
 
 
 if __name__ == "__main__":
